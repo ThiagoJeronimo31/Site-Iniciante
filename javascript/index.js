fe813bec2593054a430481b5a534b6e55f3b77fe
ThiagoJeronimo31 <jeronimothiago96@gmail.com>
2022-02-14 18:12:07 -0300

    re-praticando javascript

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+var vol = 80.8
+    console.log(`A velocidade do carro é ${vol}Km/H `)
+
+if( vol > 60){
+    console.log("Você ultrapassou a velocidade permitida. MULTADO OTÁRIO!!")
+}
+
+else{
+    console.log("Dirija sempre usando cinto de segurança")
+}
